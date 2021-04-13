e95019e3f983992de5a288be76f8a6118ffa17e7
Isacoro <cisda@hotmail.com>
2021-04-13 17:36:06 +0200

    Primer commit

---
+++ src/main/java/com/isabel/aa_api_datos/AaApiDatosApplication.java
@@ -0,0 +1,13 @@
+package com.isabel.aa_api_datos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaApiDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaApiDatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isabel/aa_api_datos/AaApiDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isabel.aa_api_datos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaApiDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
